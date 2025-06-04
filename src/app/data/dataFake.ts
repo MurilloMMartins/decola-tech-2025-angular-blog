9548765df253a69cb87e5fc98ce10c8f3359100b
MurilloMMartins <murillomartins19@gmail.com>
2025-06-04 13:45:41 -0300

    Made content page dynamic

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,14 @@
 export const dataFake = [
   {
     id: 1,
-    photo: 'aaa',
-    title: 'Teste Título',
-    description: 'Teste',
+    title: 'SAIU NOVA VERSÃO DO ANGULAR',
+    description: 'Teste teste teste',
+    photoCover: 'https://angular.jp/assets/images/press-kit/angular_wordmark_gradient.png',
   },
   {
     id: 2,
-    photo: 'aaa',
     title: 'Teste Título',
     description: 'Teste',
+    photoCover: 'https://coffective.com/wp-content/uploads/2018/06/default-featured-image.png.jpg',
   },
 ];
